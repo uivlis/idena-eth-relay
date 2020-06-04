86bdce4aa6cf0ee081a00c98d326c7af2397bef0
uivlis <vladsilviufarcas@gmail.com>
2020-06-04 14:18:11 +0300

    Add merkle root, relayKill and relayKillInvitee

--- contracts/IdenaEthRelay.sol
+++ contracts/IdenaEthRelay.sol
@@ -4,51 +4,29 @@ import "./BytesLib.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/cryptography/ECDSA.sol";
 
+
 contract IdenaEthRelay {
 
     using BytesLib for bytes;
     using SafeMath for uint256;
     using ECDSA for bytes32;
 
-    struct IDetails {
-        bool valid;
-        bool voted;
-        uint256 signatures;
-    }
-
     uint256 epoch;
-    mapping(uint256 => uint256) identitiesCount;
-    mapping(uint256 => mapping(address => IDetails)) public identities;
     mapping(uint256 => bytes32) roots;
+    mapping(uint256 => uint256) identitiesCount;
+    mapping(uint256 => mapping(address => bool)) public isIdentity;
 
-    constructor (bytes memory _identities, bytes32 _root) public {
-        for (uint256 i = 0; i < _identities.length; i += 20) {
-            identities[0][address(_identities.toAddress(i))] = IDetails(true, false, 0);
+    constructor (bytes memory _state) public {
+        bytes32 root;
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            root := mload(add(_state, 32))
+        }
+        roots[0] = root;
+        for (uint256 i = 32; i < _state.length; i += 20) {
+            isIdentity[0][address(_state.toAddress(i))] = true;
             identitiesCount[0]++;
         }
-        roots[0] = _root;
-    }
-
-    function verify(
-        bytes32 root,
-        bytes32 leaf,
-        bytes32[] memory proof
-    )
-    public
-    pure
-    returns (bool)
-    {
-        bytes32 computedHash = leaf;
-
-        for (uint256 i = 0; i < proof.length; i++) {
-            bytes32 proofElement = proof[i];
-            if (computedHash < proofElement) {
-                computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
-            } else {
-                computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
-            }
-        }
-        return computedHash == root;
     }
 
     function relayKillInvitee(
@@ -59,11 +37,41 @@ contract IdenaEthRelay {
         uint256 amount,
         uint256 maxFee,
         uint256 tips,
-        bytes payload,
-        bytes signature,
-        address killerIdentity
+        bytes memory payload,
+        bytes memory signature,
+        address killerIdentity,
+        bytes32[] memory proof
     ) public {
-
+        require(
+            txType == 10,
+            "Transaction type must be 'KillInviteeTx'(0xA)"
+        );
+        address signer = keccak256(
+            abi.encodePacked(
+                accountNonce,
+                epoch,
+                txType,
+                to,
+                amount,
+                maxFee,
+                tips,
+                payload
+            )
+        ).toEthSignedMessageHash().recover(signature);
+        require(
+            signer == killerIdentity,
+            "Recovered signer did not match killer identity."
+        );
+        require(
+            verify(roots[epoch], keccak256(abi.encodePacked(killerIdentity)), proof),
+            "Could not prove invitee status of killed identity."
+        );
+        require (
+            isIdentity[epoch][to],
+            "Killed identity is not valid."
+        );
+        delete isIdentity[epoch][to];
+        identitiesCount[epoch]--;
     }
 
     function relayKill(
@@ -74,50 +82,85 @@ contract IdenaEthRelay {
         uint256 amount,
         uint256 maxFee,
         uint256 tips,
-        bytes payload,
-        bytes signature,
+        bytes memory payload,
+        bytes memory signature,
         address killedIdentity
     ) public {
-        require(txType == 3, 'Transaction type must be "KillTx"(0x3)');
+        require(
+            txType == 3,
+            "Transaction type must be 'KillTx'(0x3)"
+        );
         address signer = keccak256(
-                abi.encodePacked(
-                    accountNonce,
-                    epoch,
-                    txType,
-                    to,
-                    amount,
-                    maxFee,
-                    tips,
-                    payload
-                )
-            ).recover(signature);
-        require(signer == killedIdentity, "Recovered signer did not match killed identity.");
-        delete identities[epoch][signer];
+            abi.encodePacked(
+                accountNonce,
+                epoch,
+                txType,
+                to,
+                amount,
+                maxFee,
+                tips,
+                payload
+            )
+        ).toEthSignedMessageHash().recover(signature);
+        require(
+            signer == killedIdentity,
+            "Recovered signer did not match killed identity."
+        );
+        require (
+            isIdentity[epoch][signer],
+            "Killed identity is not valid."
+        );
+        delete isIdentity[epoch][signer];
         identitiesCount[epoch]--;
     }
 
-    function relayState(bytes memory _identities, bytes memory signature) public {
-        address signer = keccak256(abi.encodePacked(_identities)).toEthSignedMessageHash().recover(signature);
-        uint256 e = epoch + 1;
-        while (e > 0 && !identities[e][signer].valid) {
-            e--;
-        }
-        require(
-            identities[e][signer].valid && !identities[e][signer].voted,
-            "Cannot vote in any epoch."
-        );
-        identities[e + 1][signer].voted = true;
-        uint256 threshold = (identitiesCount[e].mul(2) + 1).div(3);
-        address identity;
-        for (uint256 i = 0; i < _identities.length; i += 20) {
-            identity = address(_identities.toAddress(i));
-            identities[e + 1][identity].signatures++;
-            if (identities[e + 1][identity].signatures == threshold) {
-                identities[e + 1][identity].valid = true;
-                identitiesCount[e + 1]++;
-                epoch++;
+    function relayState(bytes memory _state, bytes memory signatures) public {
+        uint256 validSigs = 0;
+        uint256 threshold = (identitiesCount[epoch].mul(2) + 1).div(3);
+        for (uint i = 0; i < signatures.length; i += 65) {
+            address signer = keccak256(abi.encodePacked(_state))
+                .toEthSignedMessageHash()
+                .recover(signatures.slice(i, 65));
+            if (isIdentity[epoch][signer]) {
+                validSigs++;
             }
         }
+        require (
+            validSigs >= threshold,
+            "Not enough valid signatures to relay new state."
+        );
+        bytes32 root;
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            root := mload(add(_state, 32))
+        }
+        roots[++epoch] = root;
+        for (uint256 i = 32; i < _state.length; i += 20) {
+            isIdentity[epoch][address(_state.toAddress(i))] = true;
+            identitiesCount[epoch]++;
+        }
+    }
+
+    function verify(
+        bytes32 _root,
+        bytes32 _leaf,
+        bytes32[] memory _proof
+    )
+    internal
+    pure
+    returns (bool)
+    {
+        bytes32 computedHash = _leaf;
+
+        for (uint256 i = 0; i < _proof.length; i++) {
+            bytes32 proofElement = _proof[i];
+            if (computedHash < proofElement) {
+                computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
+            } else {
+                computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+            }
+        }
+        return computedHash == _root;
     }
 
 }
--- test/IdenaEthRelay.test.ts
+++ test/IdenaEthRelay.test.ts
@@ -1,6 +1,14 @@
 import path = require('path');
 import { should } from 'chai';
 import * as bip39 from 'bip39';
+const {
+    BN,           // Big Number support
+    constants,    // Common constants, like the zero address and largest integers
+    expectEvent,  // Assertions for emitted events
+    expectRevert, // Assertions for transactions that should fail
+  } = require('@openzeppelin/test-helpers');
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256')
 const hdkey = require('ethereumjs-wallet/hdkey');
 import { IdenaEthRelayInstance } from '../types/truffle-contracts';
 
@@ -18,15 +26,20 @@ contract('IdenaEthRelay', (accounts) => {
 
     let IdenaEthRelayInstance: IdenaEthRelayInstance;
     const identities0 = [2, 5, 6, 7, 8, 9];
-    const identities2 = [2, 5, 6, 7, 8, 9, 11];
-    const identities5 = [1, 5, 6, 7, 8, 9, 11];
-    const identities7 = [1, 5, 6, 7, 8, 9, 11];
-    const identities8 = [1, 5, 6, 7, 8, 10, 11];
-    const identities9 = [2, 5, 6, 7, 8, 9, 11];
     const identities00 = [5, 6, 7, 8, 9, 11];
 
-    beforeEach(async () => {
-        IdenaEthRelayInstance = await IdenaEthRelay.new("0x" + accounts.map((account, i) => identities0.includes(i) ? account.slice(2) : "").join(""));
+    const leaves0 = [5, 2, -1, 5, -1, 6, -1, 7, 7, 8, 6, 9];
+    const hashedLeaves0 = leaves0.map((leaf, i) => leaf == -1 ? "0x0" : accounts[leaf]).map(leaf => keccak256(leaf));
+    const tree0 = new MerkleTree(hashedLeaves0, keccak256, { sort: true });
+    const state0 = tree0.getHexRoot() + identities0.map((identity) => accounts[identity].slice(2)).join("");
+
+    const leaves00 = [-1, 5, -1, 6, -1, 7, -1, 8, 6, 9, 7, 11];
+    const hashedLeaves00 = leaves00.map((leaf, i) => leaf == -1 ? "0x0" : accounts[leaf]).map(leaf => keccak256(leaf));
+    const tree00 = new MerkleTree(hashedLeaves00, keccak256, { sort: true });
+    const state00 = tree00.getHexRoot() + identities00.map((identity) => accounts[identity].slice(2)).join("");
+    
+    beforeEach(async () => {  
+        IdenaEthRelayInstance = await IdenaEthRelay.new(state0);
     });
 
     /**
@@ -34,9 +47,9 @@ contract('IdenaEthRelay', (accounts) => {
      * @test {IdenaEthRelay#}
      */
     it('...should be deployed succesfully', async () => {
-        // all addresses in accounts should be valid identities
+        // check identities
         for (let i in identities0) {
-            ((await IdenaEthRelayInstance.identities("0", accounts[identities0[i]]))[0]).should.be.equal(true);
+            ((await IdenaEthRelayInstance.isIdentity("0", accounts[identities0[i]]))).should.be.equal(true);
         }
     });
 
@@ -45,33 +58,109 @@ contract('IdenaEthRelay', (accounts) => {
      * @test {IdenaEthRelay#relayState}
      */
     it('...should relay identities succesfully', async () => {
-        const message2 = "0x" + accounts.map((account, i) => identities2.includes(i) ? account.slice(2) : "").join("");
-        const message5 = "0x" + accounts.map((account, i) => identities5.includes(i) ? account.slice(2) : "").join("");
-        const message7 = "0x" + accounts.map((account, i) => identities7.includes(i) ? account.slice(2) : "").join("");
-        const message8 = "0x" + accounts.map((account, i) => identities8.includes(i) ? account.slice(2) : "").join("");
-        const message9 = "0x" + accounts.map((account, i) => identities9.includes(i) ? account.slice(2) : "").join("");
 
         const seed = await bip39.mnemonicToSeed(process.env.GANACHE_MNEMONIC ? process.env.GANACHE_MNEMONIC : "");
         const hdk = hdkey.fromMasterSeed(seed);
 
-        const addr_node2 = hdk.derivePath("m/44'/60'/0'/0/2"); //m/44'/60'/0'/0/0 is derivation path for the first account. m/44'/60'/0'/0/1 is the derivation path for the second account and so on
-        const private_key2 = addr_node2.getWallet().getPrivateKey();
-        const addr_node5 = hdk.derivePath("m/44'/60'/0'/0/5");
-        const private_key5 = addr_node5.getWallet().getPrivateKey();
-        const addr_node7 = hdk.derivePath("m/44'/60'/0'/0/7");
-        const private_key7 = addr_node7.getWallet().getPrivateKey();
-        const addr_node8 = hdk.derivePath("m/44'/60'/0'/0/8");
-        const private_key8 = addr_node8.getWallet().getPrivateKey();
-        const addr_node9 = hdk.derivePath("m/44'/60'/0'/0/9");
-        const private_key9 = addr_node9.getWallet().getPrivateKey()
+        const signers = [5, 7, 8, 9];
+        //m/44'/60'/0'/0/0 is derivation path for the first account. m/44'/60'/0'/0/1 is the derivation path for the second account and so on
+        const pks = signers.map(signer => hdk.derivePath("m/44'/60'/0'/0/" + signer).getWallet().getPrivateKey());
 
-        await IdenaEthRelayInstance.relayState(message2, web3.eth.accounts.sign(web3.utils.soliditySha3(message2), "0x" + private_key2.toString('hex')).signature, { from: accounts[2] });
-        await IdenaEthRelayInstance.relayState(message5, web3.eth.accounts.sign(web3.utils.soliditySha3(message5), "0x" + private_key5.toString('hex')).signature, { from: accounts[5] });
-        await IdenaEthRelayInstance.relayState(message7, web3.eth.accounts.sign(web3.utils.soliditySha3(message7), "0x" + private_key7.toString('hex')).signature, { from: accounts[7] });
-        await IdenaEthRelayInstance.relayState(message8, web3.eth.accounts.sign(web3.utils.soliditySha3(message8), "0x" + private_key8.toString('hex')).signature, { from: accounts[8] });
-        await IdenaEthRelayInstance.relayState(message9, web3.eth.accounts.sign(web3.utils.soliditySha3(message9), "0x" + private_key9.toString('hex')).signature, { from: accounts[9] });
+        await IdenaEthRelayInstance.relayState(state00, "0x" + pks.map(pk => web3.eth.accounts.sign(web3.utils.soliditySha3(state00), "0x" + pk.toString('hex')).signature.slice(2)).join(""), { from: accounts[1] });
+
+        // check new identities
         for (let i in identities00) {
-            ((await IdenaEthRelayInstance.identities("1", accounts[identities00[i]]))[0]).should.be.equal(true);
+            ((await IdenaEthRelayInstance.isIdentity("1", accounts[identities00[i]]))).should.be.equal(true);
         }
+    });
+
+    /**
+     * Test the killing of identitites
+     * @test {IdenaEthRelay#relayKill}
+     */
+    it('...should relay kills succesfully', async () => {
+
+        const seed = await bip39.mnemonicToSeed(process.env.GANACHE_MNEMONIC ? process.env.GANACHE_MNEMONIC : "");
+        const hdk = hdkey.fromMasterSeed(seed);
+
+        const suicider = 5;
+
+        const pk =  hdk.derivePath("m/44'/60'/0'/0/" + suicider).getWallet().getPrivateKey();
+        const tx = [
+            { type: "uint32", value: "0" }, // accountNonce
+            { type: "uint16", value: "0" }, // epoch
+            { type: "uint16", value: "3" }, // txType (KillTx)
+		    { type: "address", value: accounts[suicider] }, // to
+            { type: "uint256", value: "0" }, // amount
+            { type: "uint256", value: "0" }, // maxFee
+            { type: "uint256", value: "0" }, // tips
+            { type: "bytes", value: web3.utils.utf8ToHex("0") }, //payload
+        ];
+
+        // check identity before killing
+        ((await IdenaEthRelayInstance.isIdentity("0", accounts[suicider]))).should.be.equal(true);
+
+        await IdenaEthRelayInstance.relayKill(
+            tx[0].value,
+            tx[1].value,
+            tx[2].value,
+            tx[3].value,
+            tx[4].value,
+            tx[5].value,
+            tx[6].value,
+            tx[7].value,
+            web3.eth.accounts.sign(web3.utils.soliditySha3(...tx), "0x" + pk.toString('hex')).signature,
+            accounts[suicider],
+            { from: accounts[1] }
+        );
+
+        // check killed identity
+        ((await IdenaEthRelayInstance.isIdentity("0", accounts[suicider]))).should.be.equal(false);
+    });
+
+    /**
+     * Test the killing of invitees
+     * @test {IdenaEthRelay#relayKillInvitee}
+     */
+    it('...should relay invitee kills succesfully', async () => {
+
+        const seed = await bip39.mnemonicToSeed(process.env.GANACHE_MNEMONIC ? process.env.GANACHE_MNEMONIC : "");
+        const hdk = hdkey.fromMasterSeed(seed);
+
+        const killer = 5;
+        const killed = 2;
+
+        const pk =  hdk.derivePath("m/44'/60'/0'/0/" + killer).getWallet().getPrivateKey();
+        const tx = [
+            { type: "uint32", value: "0" }, // accountNonce
+            { type: "uint16", value: "0" }, // epoch
+            { type: "uint16", value: "10" }, // txType (KillTx)
+		    { type: "address", value: accounts[killed] }, // to
+            { type: "uint256", value: "0" }, // amount
+            { type: "uint256", value: "0" }, // maxFee
+            { type: "uint256", value: "0" }, // tips
+            { type: "bytes", value: web3.utils.utf8ToHex("0") }, //payload
+        ];
+
+        // check identity before killing
+        ((await IdenaEthRelayInstance.isIdentity("0", accounts[killed]))).should.be.equal(true);
+
+        await IdenaEthRelayInstance.relayKillInvitee(
+            tx[0].value,
+            tx[1].value,
+            tx[2].value,
+            tx[3].value,
+            tx[4].value,
+            tx[5].value,
+            tx[6].value,
+            tx[7].value,
+            web3.eth.accounts.sign(web3.utils.soliditySha3(...tx), "0x" + pk.toString('hex')).signature,
+            accounts[killer],
+            tree0.getHexProof(keccak256(accounts[killer])),
+            { from: accounts[1] }
+        );
+
+        // check killed identity
+        ((await IdenaEthRelayInstance.isIdentity("0", accounts[killed]))).should.be.equal(false);
     });
 });
